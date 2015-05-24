5bf3ef3df1ceeb94e244c2a4490e82c3440c9235
Miguel Morales <mimoralea@gmail.com>
2015-05-24 13:04:28 -0500

    Use google style formatting

--- echoclient.c
+++ echoclient.c
@@ -11,44 +11,44 @@
 /* Be prepared accept a response of this length */
 #define BUFSIZE 4096
 
-#define USAGE								\
-     "usage:\n"								\
-     "  echoclient [options]\n"						\
-     "options:\n"							\
-     "  -s                  Server (Default: localhost)\n"		\
-     "  -p                  Port (Default: 8888)\n"			\
-     "  -m                  Message to send to server (Default: \"Hello World!\"\n" \
-     "  -h                  Show this help message\n"
+#define USAGE                                                           \
+  "usage:\n"                                                            \
+  "  echoclient [options]\n"                                            \
+  "options:\n"                                                          \
+  "  -s         Server (Default: localhost)\n"                          \
+  "  -p         Port (Default: 8888)\n"                                 \
+  "  -m         Message to send to server (Default: \"Hello World!\"\n" \
+  "  -h         Show this help message\n"
 
 /* Main ========================================================= */
 int main(int argc, char **argv) {
-     int option_char = 0;
-     char *hostname = "localhost";
-     unsigned short portno = 8888;
-     char *message = "Hello World!";
+  int option_char = 0;
+  char *hostname = "localhost";
+  unsigned int16 portno = 8888;
+  char *message = "Hello World!";
 
-     // Parse and set command line arguments
-     while ((option_char = getopt(argc, argv, "s:p:m:h")) != -1) {
-	  switch (option_char) {
-	  case 's': // server
-	       hostname = optarg;
-	       break;
-	  case 'p': // listen-port
-	       portno = atoi(optarg);
-	       break;
-	  case 'm': // server
-	       message = optarg;
-	       break;
-	  case 'h': // help
-	       fprintf(stdout, "%s", USAGE);
-	       exit(0);
-	       break;
-	  default:
-	       fprintf(stderr, "%s", USAGE);
-	       exit(1);
-	  }
-     }
+  // Parse and set command line arguments
+  while ((option_char = getopt(argc, argv, "s:p:m:h")) != -1) {
+    switch (option_char) {
+      case 's':  // server
+        hostname = optarg;
+        break;
+      case 'p':  // listen-port
+        portno = atoi(optarg);
+        break;
+      case 'm':  // server
+        message = optarg;
+        break;
+      case 'h':  // help
+        fprintf(stdout, "%s", USAGE);
+        exit(0);
+        break;
+      default:
+        fprintf(stderr, "%s", USAGE);
+        exit(1);
+    }
+  }
 
-     /* Socket Code Here */
-
+  /* Socket Code Here */
+  
 }
--- echoserver.c
+++ echoserver.c
@@ -11,39 +11,39 @@
 
 #define BUFSIZE 4096
 
-#define USAGE							\
-     "usage:\n"							\
-     "  echoserver [options]\n"					\
-     "options:\n"						\
-     "  -p                  Port (Default: 8888)\n"		\
-     "  -n                  Maximum pending connections\n"	\
-     "  -h                  Show this help message\n"
+#define USAGE                                           \
+  "usage:\n"                                            \
+  "  echoserver [options]\n"                            \
+  "options:\n"                                          \
+  "  -p                  Port (Default: 8888)\n"        \
+  "  -n                  Maximum pending connections\n" \
+  "  -h                  Show this help message\n"
 
 
 int main(int argc, char **argv) {
-     int option_char;
-     int portno = 8888; /* port to listen on */
-     int maxnpending = 5;
+  int option_char;
+  int portno = 8888; /* port to listen on */
+  int maxnpending = 5;
 
-     // Parse and set command line arguments
-     while ((option_char = getopt(argc, argv, "p:n:h")) != -1){
-	  switch (option_char) {
-	  case 'p': // listen-port
-	       portno = atoi(optarg);
-	       break;
-	  case 'n': // server
-	       maxnpending = atoi(optarg);
-	       break;
-	  case 'h': // help
-	       fprintf(stdout, "%s", USAGE);
-	       exit(0);
-	       break;
-	  default:
-	       fprintf(stderr, "%s", USAGE);
-	       exit(1);
-	  }
-     }
+  // Parse and set command line arguments
+  while ((option_char = getopt(argc, argv, "p:n:h")) != -1) {
+    switch (option_char) {
+      case 'p':  // listen-port
+        portno = atoi(optarg);
+        break;
+      case 'n':  // server
+        maxnpending = atoi(optarg);
+        break;
+      case 'h':  // help
+        fprintf(stdout, "%s", USAGE);
+        exit(0);
+        break;
+      default:
+        fprintf(stderr, "%s", USAGE);
+        exit(1);
+    }
+  }
 
-     /* Socket Code Here */
+  /* Socket Code Here */
 
 }
